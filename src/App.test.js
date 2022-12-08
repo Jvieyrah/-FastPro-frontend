919be525f614a92a4722a8bfe3d9b90e6b76c8a1
jvieirah <me.contrate.aero@gmail.com>
2022-12-08 17:21:11 -0300

    staching current state

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,11 @@
-import { screen,
-  waitFor, 
-  waitForElementToBeRemoved 
-} from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderPath from './helpers/renderPath';
-import httpRequest from './axios/config';
 import { DefaultResponse } from './helpers/mocks';
 import axios from 'axios';
 
 describe('Valida formulári de login', () => {
-beforeEach(() => {
+  beforeEach(() => {
     localStorage.clear();
     jest.restoreAllMocks();
   });
@@ -17,55 +13,55 @@ beforeEach(() => {
   jest.spyOn(axios, 'post').mockImplementation(() => Promise.resolve(DefaultResponse));
 
   it('Será validado se ao clicar no botão "Ainda não tenho conta", é redirecionado para a rota "/cadastrar',
-  async () => {
-    renderPath("/");
-    const buttonRegister = screen.getByTestId('common_login__button-register');
-    userEvent.click(buttonRegister);
-    expect(window.location.pathname).toBe('/cadastrar');
-  });
-  
+    async () => {
+      renderPath("/");
+      const buttonRegister = screen.getByTestId('common_login__button-register');
+      userEvent.click(buttonRegister);
+      expect(window.location.pathname).toBe('/cadastrar');
+    });
+
   it('Será validado se ao navegar para a rota /, o input e o botão especificados estão presentes',
-  async () => {
-    renderPath("/");
-    const inputEmail = screen.getByTestId('common_login__input-email');
-    const inputPassword = screen.getByTestId('common_login__input-password');
-    const buttonLogin = screen.getByTestId('common_login__button-login');
-    expect(inputEmail).toBeInTheDocument();
-    expect(inputPassword).toBeInTheDocument();
-    expect(buttonLogin).toBeInTheDocument();
-    expect(buttonLogin).toBeDisabled();
-  });
-  
+    async () => {
+      renderPath("/");
+      const inputEmail = screen.getByTestId('common_login__input-email');
+      const inputPassword = screen.getByTestId('common_login__input-password');
+      const buttonLogin = screen.getByTestId('common_login__button-login');
+      expect(inputEmail).toBeInTheDocument();
+      expect(inputPassword).toBeInTheDocument();
+      expect(buttonLogin).toBeInTheDocument();
+      expect(buttonLogin).toBeDisabled();
+    });
+
   it('Será validado se ao digitar um email inválido, o botão de login é desabilitado',
-  async () => {
-    renderPath("/");
-    const inputEmail = screen.getByTestId('common_login__input-email');
-    const inputPassword = screen.getByTestId('common_login__input-password');
-    const buttonLogin = screen.getByTestId('common_login__button-login');
-    userEvent.type(inputEmail, 'email');
-    userEvent.type(inputPassword, '012345678');
-    expect(buttonLogin).toBeDisabled();
-  });
-  
+    async () => {
+      renderPath("/");
+      const inputEmail = screen.getByTestId('common_login__input-email');
+      const inputPassword = screen.getByTestId('common_login__input-password');
+      const buttonLogin = screen.getByTestId('common_login__button-login');
+      userEvent.type(inputEmail, 'email');
+      userEvent.type(inputPassword, '012345678');
+      expect(buttonLogin).toBeDisabled();
+    });
+
   it('Será validado se ao digitar um email válido e uma senha inválida, o botão de login é desabilitado',
-  async () => {
-    renderPath("/");
-    const inputEmail = screen.getByTestId('common_login__input-email');
-    const inputPassword = screen.getByTestId('common_login__input-password');
-    const buttonLogin = screen.getByTestId('common_login__button-login');
-    userEvent.type(inputEmail, 'email@teste.com');
-    userEvent.type(inputPassword, '123');
-    expect(buttonLogin).toBeDisabled();
-  });
-  
+    async () => {
+      renderPath("/");
+      const inputEmail = screen.getByTestId('common_login__input-email');
+      const inputPassword = screen.getByTestId('common_login__input-password');
+      const buttonLogin = screen.getByTestId('common_login__button-login');
+      userEvent.type(inputEmail, 'email@teste.com');
+      userEvent.type(inputPassword, '123');
+      expect(buttonLogin).toBeDisabled();
+    });
+
   it('Será validado se ao digitar um email válido e uma senha válida, o botão de login é habilitado',
-  async () => {
-    renderPath("/");
-    const inputEmail = screen.getByTestId('common_login__input-email');
-    const inputPassword = screen.getByTestId('common_login__input-password');
-    const buttonLogin = screen.getByTestId('common_login__button-login');
-    userEvent.type(inputEmail, 'email@teste.com');
-    userEvent.type(inputPassword, 'Senh@Val1d@');
-    expect(buttonLogin).toBeEnabled();
-  });
+    async () => {
+      renderPath("/");
+      const inputEmail = screen.getByTestId('common_login__input-email');
+      const inputPassword = screen.getByTestId('common_login__input-password');
+      const buttonLogin = screen.getByTestId('common_login__button-login');
+      userEvent.type(inputEmail, 'email@teste.com');
+      userEvent.type(inputPassword, 'Senh@Val1d@');
+      expect(buttonLogin).toBeEnabled();
+    });
 });
